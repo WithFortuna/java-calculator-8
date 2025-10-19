5e73a6aa374f98b12ab76e3110d563928b90f5b4
WithFortuna <rmsghchl0@naver.com>
2025-10-20 00:55:46 +0900

    feat: 입력, 구분자, 덧셈 기능을 조합하여 문자열 덧셈 계산기를 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,41 @@
 package calculator;
 
+import calculator.delimiter.DelimiterUtils;
+import calculator.input.ConsoleUserInputStrategy;
+import calculator.input.UserInputStrategy;
+import calculator.summarize.StringSummarizer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        String delimiters;
+
+        // 사용자 입력
+        input = getUserInput();
+
+        // delimiter 구성하기
+        DelimiterUtils delimiterUtils = new DelimiterUtils(input);
+        delimiters = getDelimiters(delimiterUtils);
+        input = extractInputIfCustomDelimiterExists(delimiterUtils);
+
+        // 문자열 덧셈 구하기
+        int result = StringSummarizer.sum(input, delimiters);
+
+        System.out.println("결과 : " + result);
     }
+
+    private static String getUserInput() {
+        UserInputStrategy userInputStrategy = new ConsoleUserInputStrategy();
+        String input = userInputStrategy.getUserInput();
+        return input;
+    }
+
+    private static String getDelimiters(DelimiterUtils delimiterUtils) {
+        return delimiterUtils.getDelimiters();
+    }
+
+    private static String extractInputIfCustomDelimiterExists(DelimiterUtils delimiterUtils) {
+        return delimiterUtils.getInputExceptCustomDelimiter();
+    }
+
 }
